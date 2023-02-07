33ded57684e414810c6ee0fb0678e6903a6a7678
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-07 09:11:14 +0100

    Hechos los atributos, getters y setters en la clase Clientes.

--- src/clases/Clientes.java
+++ src/clases/Clientes.java
@@ -1,5 +1,46 @@
 package clases;
 
 public class Clientes {
-
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private String direccion;
+	private String localidad;
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	
+	@Override
+	public String toString() {
+		return "Clientes [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", direccion=" + direccion
+				+ ", localidad=" + localidad + "]";
+	}
+	
 }
