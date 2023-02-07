1195bb4b673c64b5e3b59db48af353e6ec820065
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-07 09:13:59 +0100

    Hechos los atributos, getters y setters en la clase Habitaciones.

--- src/clases/Habitaciones.java
+++ src/clases/Habitaciones.java
@@ -1,5 +1,46 @@
 package clases;
 
 public class Habitaciones {
-
+	private int id;
+	private int id_hotel;
+	private String numero;
+	private String descripcion;
+	private double precio;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId_hotel() {
+		return id_hotel;
+	}
+	public void setId_hotel(int id_hotel) {
+		this.id_hotel = id_hotel;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	@Override
+	public String toString() {
+		return "Habitaciones [id=" + id + ", id_hotel=" + id_hotel + ", numero=" + numero + ", descripcion="
+				+ descripcion + ", precio=" + precio + "]";
+	}
 }
