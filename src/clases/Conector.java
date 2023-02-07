84ba8b29d5a6c406af1ed2064dd4d7cb890ad1d2
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-07 09:15:04 +0100

    Creada la clase Conector.

---
+++ src/clases/Conector.java
@@ -0,0 +1,35 @@
+package clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector {
+protected Connection con;
+	
+	public void conectar() {
+		try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				String url = "jdbc:mysql://localhost/hotel";
+				con = (Connection) DriverManager.getConnection(url, "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void cerrar() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getCon() {
+		return con;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+}
