36889074725e57b460605a5e57ce14840e9de5c1
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-07 09:12:12 +0100

    Hechos los atributos, getters y setters en la clase Reservas.

--- src/clases/Reservas.java
+++ src/clases/Reservas.java
@@ -1,5 +1,49 @@
 package clases;
 
-public class Reservas {
+import java.sql.Date;
 
+public class Reservas {
+	private int id;
+	private int habitacion;
+	private String dni;
+	private Date desde;
+	private Date hasta;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getHabitacion() {
+		return habitacion;
+	}
+	public void setHabitacion(int habitacion) {
+		this.habitacion = habitacion;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public Date getDesde() {
+		return desde;
+	}
+	public void setDesde(Date desde) {
+		this.desde = desde;
+	}
+	public Date getHasta() {
+		return hasta;
+	}
+	public void setHasta(Date hasta) {
+		this.hasta = hasta;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Reservas [id=" + id + ", habitacion=" + habitacion + ", dni=" + dni + ", desde=" + desde + ", hasta="
+				+ hasta + "]";
+	}
 }
