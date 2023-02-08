4b53c961fadb1e57c390b48066ef2f6c70827bc8
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-08 13:39:58 +0100

    - Hecho el metodo realizarReservas en la clase GestorBBDD

--- src/clases/GestorBBDD.java
+++ src/clases/GestorBBDD.java
@@ -1,5 +1,29 @@
 package clases;
 
-public class GestorBBDD {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class GestorBBDD extends Conector{
+		
+	PreparedStatement pst;
+	
+	public void realizarReservas (Reserva reserva) throws SQLException {
+		
+		Cliente cliente = new Cliente();
+		Habitacion habitacion = new Habitacion();
+		
+		pst = con.prepareStatement("INSERT INTO reservas (id, id_habitacion, dni, desde, hasta) VALUES (?,?,?,?,?)");
+		
+		pst.setInt(1, reserva.getId());
+		pst.setInt(2, habitacion.getId());
+		pst.setString(3, cliente.getDni());
+		pst.setDate(4, new java.sql.Date(reserva.getDesde().getTime()));
+		pst.setDate(5, new java.sql.Date(reserva.getHasta().getTime()));
+		
+		pst.execute();
+		
+		System.out.println("La reserva se ha realizado con exito!");
+		
+	}
+	
 }
