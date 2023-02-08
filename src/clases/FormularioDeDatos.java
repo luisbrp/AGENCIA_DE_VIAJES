dfb18737b832106949b5b2d7f8013017d5e3af5f
Irene Herrero <ireneherrero@outlook.com>
2023-02-08 14:19:29 +0100

    rama hoteles 1

--- src/clases/FormularioDeDatos.java
+++ src/clases/FormularioDeDatos.java
@@ -1,6 +1,81 @@
 package clases;
 
+import java.util.Scanner;
+
 public class FormularioDeDatos {
 
+	//formulario hotel
+	public static Hotel pedirDatosHotel(Scanner scan) {
+		Hotel hotel = new Hotel();
+		
+		String cif;
+		String nombre;
+		String gerente;
+		int estrellas;
+		String compania;
+		
+		scan.nextLine();
+		
+		System.out.println("Introduce el cif de hotel");
+		cif= scan.nextLine();
+		
+		System.out.println("Introduce el nombre del hotel");
+		nombre= scan.nextLine();
+		
+		System.out.println("Introduce el gerente del hotel");
+		gerente= scan.nextLine();
+		
+		System.out.println("Introduce las estrellas de hotel");
+		estrellas= scan.nextInt();
+		
+		System.out.println("Introduce la compañia del hotel");
+		compania= scan.nextLine();
+		
+		hotel.setCif(cif);
+		hotel.setNombre(nombre);
+		hotel.setGerente(gerente);
+		hotel.setEstrellas(estrellas);
+		hotel.setCif(compania);
+		
+		return hotel;
+	}
+	
+	public static Habitacion pedirDatosHabitacion(Scanner scan) {
+		
+		Habitacion habitacion = new Habitacion();
+		
+		int idHotel;
+		String numero;
+		String descripcion;
+		Double precio;
+		
+		scan.nextLine();
+		
+		System.out.println("Introduce el id del hotel");
+		idHotel= scan.nextInt();
+		
+		System.out.println("Introduce el numero de habitacion");
+		scan.nextLine();
+		numero= scan.nextLine();
+		
+		System.out.println("Introduce la descripcion de la habitación");
+		scan.nextLine();
+		descripcion= scan.nextLine();
+		
+		System.out.println("Introduce el precio de la habitación");
+		precio= scan.nextDouble();
+		
+		habitacion.setId_hotel(idHotel);
+		habitacion.setNumero(numero);
+		habitacion.setDescripcion(descripcion);
+		habitacion.setPrecio(precio);
+		
+		return habitacion;
+		
+		
+	}
+	
+	//
+	
 
 }
--- src/clases/GestorBBDD.java
+++ src/clases/GestorBBDD.java
@@ -1,5 +1,42 @@
 package clases;
 
-public class GestorBBDD {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class GestorBBDD extends Conector {
+	
+	PreparedStatement preparedSt;
+	
+	public void insertarHotel(Hotel hotel) {
+		try {
+			preparedSt=con.prepareStatement ("INSERT INTO hoteles(`id`, `cif`,"
+					+ "nombre`, `gerente`, `estrellas`, `compania`)" 
+					+"VALUES (?,?,?,?,?,?)");
+			preparedSt.setInt(1, hotel.getId());
+			preparedSt.setString(2, hotel.getCif());
+			preparedSt.setString(3, hotel.getNombre());
+			preparedSt.setString(4, hotel.getGerente());
+			preparedSt.setInt(5, hotel.getEstrellas());
+			preparedSt.setString(6, hotel.getCompania());
+			
+			preparedSt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void insertarHabitacion(Habitacion habitacion) {
+		
+		try {
+			preparedSt= con.prepareStatement("INSERT INTO `habitaciones`(`id`, `id_hotel`, "
+					+ "`numero`, `descripcion`, `precio`)"
+					+ "VALUES (?,?,?,?,?)");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 
 }
