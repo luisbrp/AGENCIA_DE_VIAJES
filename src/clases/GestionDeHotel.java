2301df13c8a87f430f59b99eda651678bc8633d4
Irene <ireneherrero@outlook.com>
2023-02-08 20:32:16 +0100

    subir cabio

--- src/clases/GestionDeHotel.java
+++ src/clases/GestionDeHotel.java
@@ -1,5 +1,34 @@
 package clases;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GestionDeHotel {
 
+	public static void run(Scanner scan) {
+		
+		int opcion;
+		GestorBBDD gestorBBDD = new GestorBBDD();
+		Hotel hotel = new Hotel();
+		int id_hotel;
+		ArrayList <Hotel> hoteles = new ArrayList <Hotel>();
+		
+		do {
+			Menu.mostrarMenuHotel();
+			opcion= scan.nextInt();
+			
+			switch(opcion) {
+			
+			case Menu.ALTA_DE_HOTEL:
+				hotel=FormularioDeDatos.pedirDatosHotel(scan);
+				gestorBBDD.conectar();
+				gestorBBDD.insertarHotel(hotel);
+				gestorBBDD.cerrar();
+				break;
+			
+			}
+		} while (opcion != Menu.SALIR);
+		
+	}
+	
 }
