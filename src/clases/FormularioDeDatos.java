58d1e68f29533012708a44b5d7256e2b2636cfdc
Santiago Martin <Santiago Martin@host.docker.internal>
2023-02-08 13:09:42 +0100

    pedirdatoscliente echo

--- src/clases/FormularioDeDatos.java
+++ src/clases/FormularioDeDatos.java
@@ -1,6 +1,27 @@
 package clases;
 
+import java.util.Scanner;
+
+import Clases.Libro;
+
 public class FormularioDeDatos {
 
-
+	//PEDIR DATOS CLIENTE
+	public static Cliente PedirDatosCliente(Scanner scan) {
+		
+		Cliente cliente = new Cliente();
+		System.out.println("Introduce el Dni del cliente");
+		cliente.setDni(scan.nextLine());
+		
+		System.out.println("Introduce el nombre del cliente");
+		cliente.setNombre(scan.nextLine());
+		
+		System.out.println("Introduce el apellido del cliente");
+		cliente.setApellidos(scan.nextLine());
+		
+		System.out.println("introduce el numero de paginas del libro");
+		cliente.setdireccion(scan.nextLine());
+		
+		return cliente;
+	}
 }
