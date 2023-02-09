1acef219a714cb7d7d662e09ac4e4e1550fd301f
Luis Fernando Mosquera <Luis Fernando Mosquera@HZ114820.H012108.NET>
2023-02-09 12:46:09 +0100

    Hecho el menu, con algunas funciones.

--- src/clases/GestionDeReservas.java
+++ src/clases/GestionDeReservas.java
@@ -1,5 +1,42 @@
 package clases;
 
-public class GestionDeReservas {
+import java.sql.SQLException;
+import java.util.Scanner;
 
+public class GestionDeReservas {
+	
+	public static void run(Scanner scan) throws SQLException {
+
+		
+		GestorBBDD gbd = new GestorBBDD();
+		Reserva reserva = new Reserva();
+		int opcion_menu;
+
+		do {
+			Menu.mostrarMenuHacerReserva();
+			opcion_menu=Integer.parseInt(scan.nextLine());
+			switch (opcion_menu) {
+			
+			case Menu.REALIZAR_RESERVA:
+				reserva = FormularioDeDatos.pedirDatosReserva(scan);
+				gbd.conectar();
+				gbd.realizarReservas();
+				gbd.cerrar();
+				break;
+			case Menu.ANULAR_RESERVA:
+				int id = FormularioDeDatos.PedirIdReserva(scan);
+				gbd.conectar();
+				gbd.anularReserva(id);
+				gbd.cerrar();
+				
+				break;
+			case Menu.SALIR:
+				
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+		} while (opcion_menu != Menu.SALIR);
+		
+	}
 }
