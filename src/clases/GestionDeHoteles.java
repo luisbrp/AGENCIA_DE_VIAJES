f3ec55e5830d5cf45c2582ca5276ec25f5b8eb13
Irene Herrero <ireneherrero@outlook.com>
2023-02-15 14:03:49 +0100

    subir

--- src/clases/GestionDeHoteles.java
+++ src/clases/GestionDeHoteles.java
@@ -16,8 +16,6 @@ public class GestionDeHoteles {
 		hotel = FormularioDeDatos.pedirDatosHotel(scan);
 		gestorBBDD.conectar();
 		gestorBBDD.insertarHotel(hotel);
-		Visor.mostrarHotel(hotel);
-		
 		gestorBBDD.cerrar();
 		
 		
@@ -36,7 +34,6 @@ public class GestionDeHoteles {
 					habitacion = FormularioDeDatos.pedirDatosHabitacion(scan);
 					gestorBBDD.conectar();
 					gestorBBDD.insertarHabitacion(habitacion);
-					Visor.mostrarHabitacion(habitacion);
 					gestorBBDD.cerrar();
 					
 					break;
